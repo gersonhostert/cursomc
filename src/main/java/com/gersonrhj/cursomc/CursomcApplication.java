e5e34bf4f5e494bd43964324ae30d62454f3be81
Gerson Hostert <gerson.hostert@gmail.com>
2021-04-13 04:38:20 -0300

    Projeto criado

---
+++ src/main/java/com/gersonrhj/cursomc/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.gersonrhj.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
